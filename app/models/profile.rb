ac8e02067c9141109ab3eeb49af5003e013ba5f2
Ian <ian@iancmacdonald.com>
2015-07-02 09:30:06 -0700

    Add full_name method to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,8 @@ class Profile < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -6,4 +6,11 @@ describe Profile do
   it { should validate_presence_of :bio }
 
   it { should have_and_belong_to_many  :categories}
+
+  describe "#full_name" do
+    it "will return the full name of the profile" do
+      profile = Profile.create(first_name: "Ian", last_name: "MacDonald", bio: "Loves to code")
+      expect(profile.full_name).to eq "Ian MacDonald"
+    end
+  end
 end
