a28c086d31bc7661de7672fa11ffc399920043a2
Ian <ian@iancmacdonald.com>
2015-07-02 16:16:43 -0700

    Add method to filter projects for a profile by category

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,6 +6,19 @@ class Profile < ActiveRecord::Base
   validates :last_name, presence: true
   validates :bio, presence: true
 
+  def projects_with_category(category)
+    return_projects = []
+    projects.each do |project|
+# binding.pry
+      return_projects.push(project) if project.categories.include?(category)
+    end
+    return_projects
+  end
+
+  # pro.categories.joins(:projects).where(id: 6) pro.categories.joins(:projects).where(id: category.id)
+  # Category.joins(:projects).where(projects: { categories: category }) }
+  # Category.joins(:projects).where(projects: { category: category })
+
   def full_name
     "#{first_name} #{last_name}"
   end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -18,7 +18,14 @@ describe Profile do
   context "#projects_with_category" do
     it "will return and array of projects for a user that are in a certain category" do
       profile = Profile.create(first_name: "Ian", last_name: "MacDonald", bio: "Loves to code")
-      
+      category_0 = Category.create(language: "Ruby", description: "Great")
+      category_1 = Category.create(language: "JS", description: "Good")
+      project_0 = Project.create(name: "Clinical Trials", description: "An app to schedule in clinical trials", repo_URL: "https://github.com/matchbookmac/clinical_trials")
+      project_1 = Project.create(name: "nick CAGE", description: "CaRAZY", repo_URL: "https://github.com/matchbookmac/nick_CAGE")
+      profile.categories.push(category_0)
+      project_0.categories.push(category_0)
+      profile.projects.push(project_0)
+      expect(profile.projects_with_category(category_0)).to eql [project_0]
     end
   end
 end
